aa07f6fff63a5c28d9c4ea8b8a94c127707f90a3
PaperRabbit <developpaperrabbit.world>
2024-09-03 11:23:33 +0530

    timing managed and reduces the size of bg image

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import './App.css'
 import Derlin from './Components/Derlin'
 import star from './assets/star.png' 
+import bgImage from './assets/background.jpg'
 
 function App() {
   
@@ -16,7 +17,7 @@ setHovered(true);
   }
 
   return (
-    <div className='w-screen h-screen relative  bg-cover bg-center flex justify-center items-center overflow-x-hidden' style={{ backgroundImage: `url("https://images.pexels.com/photos/956999/milky-way-starry-sky-night-sky-star-956999.jpeg?cs=srgb&dl=pexels-felixmittermeier-956999.jpg&fm=jpg")` }}>
+    <div className='w-screen h-screen relative  bg-cover bg-center flex justify-center items-center overflow-x-hidden' style={{ backgroundImage: `url(${bgImage})`}}>
       <div className='absolute top-[-35%] left-[70%] z-50 animate-move-diagonal  r '>
         <img src="https://i.pinimg.com/originals/1a/8f/45/1a8f45557f4df6b9e0adf351d26a20ba.png" className='rotate-[-22deg] z-40   ' alt="" />
       </div>
